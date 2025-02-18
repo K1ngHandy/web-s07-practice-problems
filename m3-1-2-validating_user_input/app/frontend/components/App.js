2dd866d7f309826d7cc759b2f33bf26a46e5f052
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-18 11:21:45 -0500

    npm

--- m3-1-2-validating_user_input/app/frontend/components/App.js
+++ m3-1-2-validating_user_input/app/frontend/components/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import * as yup from 'yup'
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
 
 // PROBLEM 1 - Fix the `formSchema`. Once it's working, the disabled button should enable itself when "username" has at least three characters in length, and when "fun" is a boolean with a value of true.
 
@@ -18,55 +18,61 @@ const formSchema = yup.object().shape({
   .oneOf([true], "You must have fun!")
 
   */
-})
+});
 
 export default function App() {
-  let [values, setValues] = useState({ username: '', fun: false })
-  let [errors, setErrors] = useState({ username: '', fun: '' })
-  let [canSubmit, setCanSubmit] = useState(false)
+  let [values, setValues] = useState({ username: "", fun: false });
+  let [errors, setErrors] = useState({ username: "", fun: "" });
+  let [canSubmit, setCanSubmit] = useState(false);
 
   useEffect(() => {
-    formSchema.isValid(values).then(setCanSubmit)
-  }, [values])
+    formSchema.isValid(values).then(setCanSubmit);
+  }, [values]);
 
-  const onSubmit = event => {
-    event.preventDefault()
-    console.log('SUBMITTING', values)
-  }
-  const onChange = event => {
-    let { type, name, checked, value } = event.target
-    if (type == 'checkbox') value = checked
-    setValues({ ...values, [name]: value })
-    yup.reach(formSchema, name).validate(value)
-      .then(() => setErrors({ username: '', fun: '' }))
-      .catch(e => {
-        console.log(e.errors[0])
-        setErrors({ ...errors, [name]: e.errors[0] })
-      })
-  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("SUBMITTING", values);
+  };
+  const onChange = (event) => {
+    let { type, name, checked, value } = event.target;
+    if (type == "checkbox") value = checked;
+    setValues({ ...values, [name]: value });
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => setErrors({ username: "", fun: "" }))
+      .catch((e) => {
+        console.log(e.errors[0]);
+        setErrors({ ...errors, [name]: e.errors[0] });
+      });
+  };
   return (
     <form onSubmit={onSubmit}>
       <div>
-        <label>Username <input
-          type="text"
-          name="username"
-          value={values.username}
-          onChange={onChange}
-        />
+        <label>
+          Username{" "}
+          <input
+            type="text"
+            name="username"
+            value={values.username}
+            onChange={onChange}
+          />
         </label>
         {/* 2- display username validation error here */}
       </div>
       <div>
-        <label>Having fun <input
-          name="fun"
-          type="checkbox"
-          onChange={onChange}
-          checked={values.fun}
-        />
+        <label>
+          Having fun{" "}
+          <input
+            name="fun"
+            type="checkbox"
+            onChange={onChange}
+            checked={values.fun}
+          />
         </label>
         {/* 2- display fun validation error here */}
       </div>
       <input disabled={!canSubmit} type="submit" />
     </form>
-  )
+  );
 }
